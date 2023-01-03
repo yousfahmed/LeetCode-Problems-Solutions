dee09b716eaedc045f73d0c6c0a0858798367be2
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2023-01-03 04:30:24 +0200

    Time: 87 ms (49.83%), Space: 17.9 MB (15.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,17 +1,51 @@
+struct segmentTree {
+  vector<int> seg;
+  int sz;
+
+  void update(int l, int r, int v, int x, int lx, int rx) {
+    if (l <= lx and rx <= r) {
+      seg[x] = min(seg[x], v);
+      return;
+    }
+    if (r <= lx or rx <= l)return;
+    int m = (rx + lx) >> 1;
+    update(l, r, v, x * 2 + 1, lx, m);
+    update(l, r, v, x * 2 + 2, m, rx);
+  }
+
+  segmentTree(int n) {
+    sz = 1;
+    while (sz < n)sz <<= 1;
+    seg = vector<int>(sz << 1, 1e9);
+  }
+
+  int query(int i, int x, int lx, int rx) {
+    if (rx - lx == 1) return seg[x];
+    int m = (rx + lx) >> 1;
+    int res = seg[x];
+    if (i < m) res = min(res, query(i, x * 2 + 1, lx, m));
+    else res = min(res, query(i, x * 2 + 2, m, rx));
+    return res;
+  }
+
+  void update(int l, int r, int v) {
+    update(l, r, v, 0, 0, sz);
+  }
+
+  int query(int i) {
+    return query(i, 0, 0, sz);
+  }
+};
+
+
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        const int n = nums.size();
-        int dp[n];
-        memset(dp,63,sizeof dp);
-        const int inf = dp[0];
-        dp[0] = 0;
-
-        for(int i = 0 ; i < n ; ++i){
-            for(int j = 1 ; j <= nums[i] && j + i < n; ++j){
-                dp[j+i] = min(dp[i+j],dp[i]+1);
-            }
-        }
-        return dp[n-1];
-    }
+  int jump(vector<int> &nums) {
+    const int n = nums.size();
+    segmentTree seg(n);
+    seg.update(0, 1, 0);
+    for (int i = 0; i < n; ++i) 
+      seg.update(i, min(n, nums[i] + i + 1), seg.query(i) + 1);
+    return seg.query(n - 1);
+  }
 };
