034ddfcd31b858f4c4cb74cfd481f5abcec76443
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2023-01-06 20:38:43 +0200

    Time: 192 ms (91.71%), Space: 76.5 MB (76.68%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  int maxIceCream(vector<int> &costs, int coins) {
+    sort(costs.begin(), costs.end());
+    for (int i = 0; i < costs.size(); ++i) {
+      if (coins < costs[i])return i;
+      coins -= costs[i];
+    }
+    return costs.size();
+  }
+};
