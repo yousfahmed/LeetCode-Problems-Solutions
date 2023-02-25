192490c5dec13fe9bf24dcf003dc61eec3886087
Abdeelaleem Ahmed <yousfahmed2@gmail.com>
2023-02-25 05:59:29 +0200

    Time: 145 ms (51.27%), Space: 93.3 MB (56.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-  int maxProfit(vector<int> &prices) {
-    const int n = prices.size();
-    reverse(prices.begin(), prices.end());
-    int mx = 0;
-    int ret = 0;
-    for (int &i: prices) {
-      ret = max(ret, mx - i);
-      if (i > mx) mx = i;
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maximumProfit = 0, minStockVal = INT_MAX;
+        int i = 0;
+        while (i < n) {
+            minStockVal = min(minStockVal, prices[i]);
+            // whenever the price of current stock is greater then then the stock value which we bought then only we will sell the stock 
+            if (prices[i] >= minStockVal)
+                maximumProfit = max(maximumProfit, prices[i] - minStockVal);
+            i++;
+        }
+        return maximumProfit;
     }
-    return ret;
-  }
 };
